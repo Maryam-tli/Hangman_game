98af919d158d9694d26cbb6ef7dba46f61485643
Maryam-tli <m.tavakoolii1400@gmail.com>
2024-11-14 00:54:55 +0330

    Initialize guessed letters string and tries counter

--- hangman.py
+++ hangman.py
@@ -3,6 +3,9 @@ from random import choice
 
 def run_game():
     word: str = choice(['eraser', 'secret', 'banana', 'phone', 'Maryam'])
+
     username: str = input('What is your name? ')
     print(f'Welcome to hangman , {username}')
 
+    guessed: str = ''
+    tries: int = 3
