0a57560da8150c056cf2d77c2ceb61a1323d4603
Maryam-tli <m.tavakoolii1400@gmail.com>
2024-11-14 00:56:18 +0330

    Implement game loop with display of guessed letters and blanks

--- hangman.py
+++ hangman.py
@@ -9,3 +9,16 @@ def run_game():
 
     guessed: str = ''
     tries: int = 3
+
+    while tries > 0:
+        blanks: int = 0
+        print('Word: ', end='')
+
+        for char in word:
+            if char in guessed:
+                print(char, end='')
+            else:
+                print('_', end='')
+                blanks += 1
+
+        print()
